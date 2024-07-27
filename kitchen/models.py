5e18f77af2657be3ab544cdc4ebec1b5d49962cd
TarasFirst <firsttaras@mail.com>
2024-07-27 12:42:34 +0300

    Create models Cook, Dish, DishType and add models.py
    Modify models.py:
    - create Cook(AbstractUser)
    - create DishType
    - create Dish with many-to-many to Cook and foreig-key to DishType.
    Add models.py.

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    # def get_absolute_url(self):
+    #     return reverse("kitchen:cook-detail", kwargs={"pk": self.pk})
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    def __str__(self):
+        return f"{self.name} ({self.dish_type})"
