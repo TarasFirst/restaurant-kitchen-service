ae9818a4e191a3170fd45071541db97f539a3c96
TarasFirst <firsttaras@mail.com>
2024-07-27 14:53:33 +0300

    Add kitchen/views.py
    - with func: index.

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.decorators import login_required
+# from django.http import HttpResponseRedirect
 from django.shortcuts import render
+# from django.urls import reverse_lazy
+# from django.views import generic
+# from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from kitchen.models import Cook, Dish, DishType
+# from taxi.forms import (
+#     DriverCreationForm,
+#     DriverLicenseUpdateForm,
+#     CarForm,
+#     CarSearchForm,
+#     DriverSearchForm,
+#     ManufacturerSearchForm,
+# )
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
