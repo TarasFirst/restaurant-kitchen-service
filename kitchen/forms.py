d58f3e461a0b83d69a07b089bba06aaadab55886
TarasFirst <firsttaras@mail.com>
2024-07-28 14:36:18 +0300

    Modify
    - forms: for Cook add update, create, search forms.

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
-from kitchen.models import Dish
+from kitchen.models import Dish, Cook
 
 
 class DishForm(forms.ModelForm):
@@ -30,4 +32,44 @@ class DishSearchForm(forms.Form):
         required=False,
         label="",
         widget=forms.TextInput(attrs={"placeholder": "Search by name"}),
+    )
+
+
+class CookCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Cook
+        fields = UserCreationForm.Meta.fields + (
+            "years_of_experience",
+            "first_name",
+            "last_name",
+        )
+
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(self.cleaned_data["years_of_experience"])
+
+
+class CookUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Cook
+        fields = ["years_of_experience"]
+
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(self.cleaned_data["years_of_experience"])
+
+
+def validate_years_of_experience(
+    years_of_experience,
+):
+    if years_of_experience < 0:
+        raise ValidationError("Years_of_experience number should be a positive number.")
+
+    return years_of_experience
+
+
+class CookSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"}),
     )
