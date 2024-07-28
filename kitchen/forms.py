b85a13f9c5bcf2e3f4cea14a4ef2018321cf5e05
TarasFirst <firsttaras@mail.com>
2024-07-28 13:17:52 +0300

    Create
    - forms: add DishForm, DishSearchForm.

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,7 +1,30 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+from kitchen.models import Dish
+
+
+class DishForm(forms.ModelForm):
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Dish
+        fields = "__all__"
 
 
 class DishTypeSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"}),
+    )
+
+
+class DishSearchForm(forms.Form):
     name = forms.CharField(
         max_length=255,
         required=False,
