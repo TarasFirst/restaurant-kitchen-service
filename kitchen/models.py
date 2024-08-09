5cd2db7d9e9a3894385e411cae592d6a87f4e535
TarasFirst <firsttaras@mail.com>
2024-08-09 19:20:19 +0300

    Fix model Dish
    - add field price to the model and to templates.

--- kitchen/models.py
+++ kitchen/models.py
@@ -35,6 +35,7 @@ class Dish(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     cooks = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
         related_name="dishes"
