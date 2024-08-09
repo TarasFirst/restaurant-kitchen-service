ba8636a438c8f593e2866cc4475ecb621e1ef92d
TarasFirst <firsttaras@mail.com>
2024-08-09 19:35:33 +0300

    Fix SECRET_KEY
    - fix data of variable SECRET_KEY.

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
