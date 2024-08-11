2201c5beb594276568d92b28b672dcbd2cba19c1
TarasFirst <firsttaras@mail.com>
2024-08-11 11:28:34 +0300

    Modify settings.py
    - fix SECRET_KEY.

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -28,8 +28,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-# DEBUG = False
-
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1"]
